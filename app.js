c90a8f0986316906fddd7488ed5d62766c56fe87
James <mirageapache@gmail.com>
2023-05-24 10:13:20 +0800

    MongoDB connect setting & create new DB

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const port = process.env.port || 3000
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 建構express應用程式Server
 const app = express()
 
+// 資料庫連線
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
 
+db.once('open', () => {console.log('mongodb connected!')})
+db.on('error', () => {console.log('mongodb error!')})
+
+// 路由設定
 app.get('/', (req,res) => {
   res.send('express server')
 })
